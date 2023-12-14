c3d9deec065da31a859fb2fa220a2ea692434dc1
Sahil Nikshe <nikshesahil@gmail.com>
2023-12-14 16:55:49 +0530

    Done with Vendor's Api

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('vendors/',listVendor.as_view()),
+    path('vendors/create/',createVendor.as_view()),
+    path('vendors/<int:pk>/detail/',vendorDetail.as_view()),
+    path('vendors/<int:pk>/update/',updateVendorDetail.as_view()),
+    path('vendors/<int:pk>/delete/',deleteVendor.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import *
 # Create your views here.
+
+class createVendor(generics.CreateAPIView):
+    serializer_class=VendorSerializer
+    queryset=Vendor.objects.all()
+
+class listVendor(generics.ListAPIView):
+    serializer_class=VendorSerializer
+    queryset=Vendor.objects.all()
+
+class vendorDetail(generics.RetrieveAPIView):
+    serializer_class=VendorSerializer
+    queryset=Vendor.objects.all()
+    lookup_field='pk'
+
+class updateVendorDetail(generics.UpdateAPIView):
+    serializer_class=VendorSerializer
+    queryset=Vendor.objects.all()
+
+class deleteVendor(generics.DestroyAPIView):
+    serializer_class=VendorSerializer
+    queryset=Vendor.objects.all()
