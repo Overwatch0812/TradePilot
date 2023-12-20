878421da4a7e95c28bd256225f769bdfa99e5c73
Sahil Nikshe <nikshesahil@gmail.com>
2023-12-21 00:26:14 +0530

    SetUp for hosting 1.5

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building project"
-python -m pip install -r requirements.txt
+python3.11.2 -m pip install -r requirements.txt
 
 echo "Make migrations"
-python manage.py makemigrations
-python manage.py migrate
+python3.11.2 manage.py makemigrations
+python3.11.2 manage.py migrate
 
 echo "Collecting static"
-python manage.py collectstatic
+python3.11.2 manage.py collectstatic
